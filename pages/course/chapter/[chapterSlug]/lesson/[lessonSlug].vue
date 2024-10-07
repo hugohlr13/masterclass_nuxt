e8b7fc92dc612776041e8388f371e14637563c65
Hugo Huet-Leroy <hugohuetleroy@hugosmacstudio.home>
2024-10-07 10:53:11 +0200

    progress useState

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -25,6 +25,10 @@
         :videoId="lesson.videoId"
       />
       <p>{{ lesson.text }}</p>
+      <LessonCompleteButton
+      :model-value="isLessonComplete"
+      @update:model-value="toggleComplete"
+    />
     </div>
   </template>
   
@@ -43,4 +47,44 @@
       (lesson) => lesson.slug === route.params.lessonSlug
     );
   });
-  </script>
+
+
+const title = computed(() => {
+  return `${lesson.value.title} - ${course.title}`;
+});
+useHead({
+    title,
+  });
+
+  const progress = useState('progress', () => {
+  return [];
+});
+
+const isLessonComplete = computed(() => {
+  if (!progress.value[chapter.value.number - 1]) {
+    return false;
+  }
+
+  if (
+    !progress.value[chapter.value.number - 1][
+      lesson.value.number - 1
+    ]
+  ) {
+    return false;
+  }
+
+  return progress.value[chapter.value.number - 1][
+    lesson.value.number - 1
+  ];
+});
+
+const toggleComplete = () => {
+  if (!progress.value[chapter.value.number - 1]) {
+    progress.value[chapter.value.number - 1] = [];
+  }
+
+  progress.value[chapter.value.number - 1][
+    lesson.value.number - 1
+  ] = !isLessonComplete.value;
+};
+</script>
