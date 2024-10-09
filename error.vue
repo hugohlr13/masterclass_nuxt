8d9a0fa237ad135e53660357dfad5f723384f52b
Hugo Huet-Leroy <hugohuetleroy@hugosmacstudio.home>
2024-10-09 16:37:29 +0200

    server errors

---
+++ error.vue
@@ -0,0 +1,14 @@
+<template>
+    <NuxtLayout>
+        <div class="prose">
+            <h1>Dang</h1>
+            <p>It looks like something broke.</p>
+            <p>Sorry about that.</p>
+            <p> <strong>{{ error.message }}</strong></p>
+        </div>
+    </NuxtLayout>
+</template>
+
+<script setup>
+const error = useError();
+</script>
